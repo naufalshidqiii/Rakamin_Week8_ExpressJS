8e6afd21613f92277c373da318e71575dd7bfb1f
Naufal Shidqi <naufalshidqi99@gmail.com>
2023-02-25 23:48:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,14 +12,6 @@ const router = require("./query.js");
 
 app.use(router);
 
-// app.get("/", (req, res) => {
-// 	res.send("Hello World! get test");
-// });
-
-// app.post("/", (req, res) => {
-// 	res.send("Hello World! post test");
-// });
-
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
 });
